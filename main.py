53aee5985d24b35a3946c3ac567718e593ea29dd
Srikumar6529 <sridivijendrakumar@gmail.com>
2026-09-07 11:13:06 -0500

    Intiliased the project

---
+++ main.py
@@ -0,0 +1,5 @@
+import pygame
+from constants import SCREEN_WIDTH,SCREEN_HEIGHT
+print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+print(f"Screen width: {SCREEN_WIDTH}")
+print(f"Screen height: {SCREEN_HEIGHT}")
